bf15e0ece1673ddb2b0a97d0700119f188dbbe4a
minhtam <zanname98@gmail.com>
2023-05-08 15:20:33 +0700

    Dựng khung layout mặc định

--- src/components/Layouts/DefaultLayot/index.js
+++ src/components/Layouts/DefaultLayot/index.js
@@ -1,19 +1,20 @@
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import classNames from 'classnames/bind';
 
-function DefaultLayout({children}) {
-   return (
-    <div className="wrapper">
-        <Header/>
-        <div className="container">
-            <Sidebar/>
-            <div className="content">
-                {children}
+import Header from '~/components/Layouts/components/Header';
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
-    </div>
-   )
-   
+    );
 }
 
 export default DefaultLayout;
